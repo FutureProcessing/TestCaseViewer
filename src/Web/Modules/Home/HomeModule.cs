f85c62939de9af2aae5063d51c04ab685640ffd5
Maciej Nowak <mnowak@future-processing.com>
2015-03-26 13:39:47 +0100

    Nancy hello world

---
+++ src/Web/Modules/Home/HomeModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Modules.Home {
+    using Nancy;
+
+    public class HomeModule : NancyModule {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World";
+        }
+    }
+}
---
+++ src/Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Web.Startup))]
+
+namespace Web {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.UseNancy();
+        }
+    }
+}
