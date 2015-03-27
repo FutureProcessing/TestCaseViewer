817c47adabfa4627de44d539e47fed6dd7e13726
Maciej Nowak <mnowak@future-processing.com>
2015-03-27 12:16:35 +0100

    Simple authentication flow

---
+++ src/Web/Modules/Authentication/AuthenticationModule.cs
@@ -0,0 +1,48 @@
+﻿namespace Web.Modules.Authentication
+{
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Security.Principal;
+    using Microsoft.Owin.Security;
+    using Nancy;
+    using Nancy.Security;
+
+    public class AuthenticationModule : BaseModule
+    {
+        public AuthenticationModule()
+            : base("/auth")
+        {
+            Post["login"] = _ =>
+            {
+                var props = new AuthenticationProperties { IsPersistent = true };
+
+                var identity = new ClaimsIdentity(new GenericIdentity("admin", "Forms"), new[]
+                {
+                    new Claim(LocalClaims.PasswordType, "password"), 
+                });
+
+                this.OwinContext.Authentication.SignIn(props, identity);
+
+                return Ok;
+            };
+
+            Post["/logout"] = _ =>
+            {
+                this.OwinContext.Authentication.SignOut("Forms");
+
+                return Ok;
+            };
+
+            Get["identity", ctx => ctx.CurrentUser.IsAuthenticated()] = _ => new
+            {
+                IsAuthenticated = true,
+                UserName = this.CurrentUser.Identity.Name
+            };
+
+            Get["identity", ctx => !ctx.CurrentUser.IsAuthenticated()] = _ => new
+            {
+                IsAuthenticated = false
+            };
+        }
+    }
+}
---
+++ src/Web/Modules/BaseModule.cs
@@ -0,0 +1,37 @@
+﻿namespace Web.Modules
+{
+    using System.Security.Claims;
+    using Microsoft.Owin;
+    using Nancy;
+    using Nancy.Owin;
+
+    public abstract class BaseModule : NancyModule
+    {
+        protected IOwinContext OwinContext
+        {
+            get { return new OwinContext(this.Context.GetOwinEnvironment()); }
+        }
+
+        protected ClaimsPrincipal CurrentUser
+        {
+            get { return this.OwinContext.Authentication.User; }
+        }
+
+        protected object Ok
+        {
+            get { return new {Ok = true}; }
+        }
+
+        protected BaseModule()
+        {
+            
+        }
+
+        protected BaseModule(string modulePath)
+            : base(modulePath)
+        {
+            
+        }   
+            
+    }
+}
