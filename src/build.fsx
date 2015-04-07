e6f918ef1d16eaea3858ed058f9259a927dce1c5
Maciej Nowak <maciejt.nowak@gmail.com>
2015-04-07 10:33:07 +0200

    Minimal Fake dist

---
+++ src/build.fsx
@@ -0,0 +1,29 @@
+﻿#r "packages/FAKE.Core.3.26.7/tools/FakeLib.dll"
+
+open Fake
+
+let packagesDir = "packages" |> FullName
+
+Target "Default" (fun _ ->    
+    DoNothing()
+)
+
+Target "Clean" (fun _ ->
+    DoNothing()
+)
+
+Target "Build" (fun _ ->    
+    "TestCaseViewer.sln" 
+        |> RestoreMSSolutionPackages(fun p-> {p with
+                                                ToolPath = FileSystem.findToolInSubPath "NuGet.exe"  packagesDir  
+                                             }
+                                    )
+
+
+)
+
+"Clean"
+==> "Build"
+==> "Default"
+
+RunTargetOrDefault "Default"
