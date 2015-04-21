cab6a1c0c02aff483824dab4b0c28127c38f621d
Łukasz Kojzar <lkojzar@future-processing.com>
2015-04-21 12:02:39 +0200

    Minor Selenium scenarios refactor
    Update app URL in AppUrls class.

--- src/TestCaseViewerSeleniumScenarios/AppUrls.cs
+++ src/TestCaseViewerSeleniumScenarios/AppUrls.cs
@@ -8,14 +8,16 @@ namespace TestCaseViewerSeleniumScenarios
 {
     public static class AppUrls
     {
-        private static string _LoginPageUrl = "http://localhost/TestCaseViewer/#/login";
+        private static string _baseAppUrl = "http://localhost/TestCaseViewer/";
+
+        private static string _LoginPageUrl = _baseAppUrl + "#/login";
 
         public static string LoginPageUrl
         {
             get { return _LoginPageUrl; }
         }
 
-        private static string _MainPageUrl = "http://localhost/TestCaseViewer/#/testcases";
+        private static string _MainPageUrl = _baseAppUrl + "#/testcases";
 
         public static string MainPageUrl
         {
